96a68ad0274d8e7a06a906d23d745c98afe53813
jgroman <jgroman@users.noreply.github.com>
2024-01-16 15:46:03 +0100

    Add cocotb test for project 01

--- 01_thruwire-onboard/src/top.v
+++ 01_thruwire-onboard/src/top.v
@@ -4,15 +4,15 @@ module top
     (
     input   wire    i_sw_s1,
     input   wire    i_sw_s2,
-    output  wire    o_led_done, 
+    output  wire    o_led_done,
     output  wire    o_led_ready
     );
-    
+
     assign  o_led_ready = i_sw_s1;
     assign  o_led_done = ~i_sw_s2;
 
    initial begin
-      $dumpfile("logs/01.vcd");
+      $dumpfile("../logs/01.vcd");
       $dumpvars();
    end
 
